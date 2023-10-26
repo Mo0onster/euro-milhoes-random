9ef07096e980ddb04a3a93bba976e8e16d03d799
Francisco Maiato <kikomaiato@gmail.com>
2023-10-26 15:32:53 +0000

    trabalho.sh

--- trabalho.sh
+++ trabalho.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-inicio=0
-tempo=5
+inicio=1
+tempo=6
 
+echo "Números aleatórios para o euro milhões!"
 while [ $inicio -ne $tempo ]
 do
+    echo "Tentativa $inicio"
     echo "O numero é $(( $RANDOM % 50 + 1))"
     echo "A estrela é $(( $RANDOM % 12 + 1))"
     ((inicio=inicio+1))

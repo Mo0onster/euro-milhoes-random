1c92ad22702ab2995311460ed48a137867b27570
Francisco Maiato <kikomaiato@gmail.com>
2023-10-27 11:23:08 +0000

    comentario

--- trabalho.sh
+++ trabalho.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 inicio=1
-tempo=6
+tempo=6     #tempo = numero de tentativas +1
 
 echo "Números aleatórios para o euro milhões!"
 while [ $inicio -ne $tempo ]
 do
     echo "Tentativa $inicio"
-    echo "O numero é $(( $RANDOM % 50 + 1))"
-    echo "A estrela é $(( $RANDOM % 12 + 1))"
+    echo "O numero é: $(( $RANDOM % 50 + 1))"
+    echo "A estrela é: $(( $RANDOM % 12 + 1))"
     ((inicio=inicio+1))
 done
